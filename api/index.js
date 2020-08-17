75ac80e5d02348aaa6c256aff9af7eb2ea76785d
katuo-sakana <katuo.webkaihatsu@gmail.com>
2020-08-18 02:09:13 +0900

    api/capsへのGET処理で、puppeteerで画像生成

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const puppeteer = require("puppeteer");
 const app = express();
 
 app.get("/test", (req, res) => {
+  res.send("API server works fine");
+});
+
+app.get("/caps", (req, res) => {
+  (async () => {
+    const browser = await puppeteer.launch(); //Chromiumを起動
+    const page = await browser.newPage(); //新しいタブを開く
+    await page.goto("https://liginc.co.jp/"); //指定したURLに移動
+    await page.screenshot({ path: "caps02.png" }); //スクリーンショットを撮る
+
+    await browser.close(); //Chromiumを閉じる
+  })();
   res.send("API server works fine");
 });
 
