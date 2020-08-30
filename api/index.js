0efb6fa7b50b94da67a34276e66c8c1dd0f3621e
katuo-sakana <katuo.webkaihatsu@gmail.com>
2020-08-30 21:18:25 +0900

    Promise.allを使用して並列処理に変更

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ app.get("/test", (req, res) => {
 });
 
 app.post("/caps", (req, res) => {
+  const DCL = { waitUntil: "domcontentloaded" };
   const requestUrl01 = req.body.urldata01;
   const requestUrl02 = req.body.urldata02;
   const requestUrls = [requestUrl01, requestUrl02];
@@ -22,17 +23,43 @@ app.post("/caps", (req, res) => {
   });
   (async () => {
     const browser = await puppeteer.launch(); //Chromiumを起動
-    const page = await browser.newPage(); //新しいタブを開く
-    await page.goto(requestUrl01); //指定したURLに移動
-    await page.screenshot({
-      path: `static/images/${capsId}/01.png`,
-      fullPage: true
-    }); //スクリーンショットを撮る
-    await page.goto(requestUrl02); //指定したURLに移動
-    await page.screenshot({
-      path: `static/images/${capsId}/02.png`,
-      fullPage: true
-    }); //スクリーンショットを撮る
+    const promiseList = [];
+    const titleList = [];
+    requestUrls.forEach((url, index) => {
+      promiseList.push(
+        (async () => {
+          const page = await browser.newPage();
+          const puppeteerRes = await page.goto(url, DCL);
+          if (puppeteerRes.status() !== 200)
+            return `${puppeteerRes.status()} ERROR`;
+
+          const result = await page.screenshot({
+            path: `static/images/${capsId}/0${index}.png`,
+            fullPage: true
+          }); //スクリーンショットを撮る
+
+          await page.close();
+          return result;
+        })().catch(e => console.error(e))
+      );
+    });
+
+    await Promise.all(promiseList).then(vList => {
+      vList.forEach(title => titleList.push(title));
+    });
+
+    // const page = await browser.newPage(); //新しいタブを開く
+    // await page.goto(requestUrl01); //指定したURLに移動
+    // await page.screenshot({
+    //   path: `static/images/${capsId}/01.png`,
+    //   fullPage: true
+    // }); //スクリーンショットを撮る
+    // await page.goto(requestUrl02); //指定したURLに移動
+    // await page.screenshot({
+    //   path: `static/images/${capsId}/02.png`,
+    //   fullPage: true
+    // }); //スクリーンショットを撮る
+
     await browser.close(); //Chromiumを閉じる
     await res.redirect(`/${capsId}`); // awaitしてリダイレクトしないとページ遷移時に画像が表示されないため。（スクリーンショットが撮り終わったタイミングの処理をvueに記述すればうまくいくかも？）
   })();
