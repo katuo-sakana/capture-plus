63e3edd268872b5ba1835022a37b2d00916a3a9a
katuo-sakana <katuo.webkaihatsu@gmail.com>
2020-08-30 20:10:54 +0900

    キャプチャをフルページ処理に変更

--- api/index.js
+++ api/index.js
@@ -16,7 +16,10 @@ app.post("/caps", (req, res) => {
     const browser = await puppeteer.launch(); //Chromiumを起動
     const page = await browser.newPage(); //新しいタブを開く
     await page.goto(requestUrl); //指定したURLに移動
-    await page.screenshot({ path: `static/images/${capsId}.png` }); //スクリーンショットを撮る
+    await page.screenshot({
+      path: `static/images/${capsId}.png`,
+      fullPage: true
+    }); //スクリーンショットを撮る
     await browser.close(); //Chromiumを閉じる
     await res.redirect(`/${capsId}`); // awaitしてリダイレクトしないとページ遷移時に画像が表示されないため。（スクリーンショットが撮り終わったタイミングの処理をvueに記述すればうまくいくかも？）
   })();
