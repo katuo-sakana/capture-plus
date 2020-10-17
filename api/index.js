d522996a6fc9c17ab15ff0847c0c34276e2b104c
katuo-sakana <katuo.webkaihatsu@gmail.com>
2020-10-17 22:09:27 +0900

    フルスクリーン,700,400の幅でスクリーンショットを撮影 #8

--- api/index.js
+++ api/index.js
@@ -13,8 +13,9 @@ app.get("/test", (req, res) => {
 app.post("/caps", (req, res) => {
   const DCL = { waitUntil: "domcontentloaded" };
   const requestUrl01 = req.body.urldata01;
-  const requestUrl02 = req.body.urldata02;
-  const requestUrls = [requestUrl01, requestUrl02];
+  const widthSets = ["full", 700, 400];
+  // const requestUrl02 = req.body.urldata02;
+  // const requestUrls = [requestUrl01, requestUrl02];
   const capsId = uuid.v4();
   fs.mkdirSync(`static/images/${capsId}`, err => {
     if (err) {
@@ -25,11 +26,17 @@ app.post("/caps", (req, res) => {
     const browser = await puppeteer.launch(); //Chromiumを起動
     const promiseList = [];
     const titleList = [];
-    requestUrls.forEach((url, index) => {
+    widthSets.forEach((widthSet, index) => {
       promiseList.push(
         (async () => {
           const page = await browser.newPage();
-          const puppeteerRes = await page.goto(url, DCL);
+          if (widthSet !== "full") {
+            await page.setViewport({
+              width: widthSet,
+              height: 768
+            });
+          }
+          const puppeteerRes = await page.goto(requestUrl01, DCL);
           if (puppeteerRes.status() !== 200)
             return `${puppeteerRes.status()} ERROR`;
 
