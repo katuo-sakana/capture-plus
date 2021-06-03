27a23c1192eff48085eac29f965fb867e3740c39
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-06-04 07:07:18 +0900

    キャプチャ用コード整理

--- api/index.js
+++ api/index.js
@@ -17,28 +17,6 @@ app.get("/test", (req, res, next) => {
   Page.findAll().then(pages => {
     res.send(pages[0].url);
   });
-  // res.send("API server works fine");
-  // var pool = new pg.Pool({
-  //   database: process.env.DB_DATABASE,
-  //   user: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   host: process.env.DB_HOST,
-  //   port: process.env.DB_PORT
-  // });
-  // pool.connect(function(err, client) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     client.query("SELECT name FROM staff", function(err, result) {
-  //       res.send(result.rows[0].name);
-  //       // res.render("index", {
-  //       //   title: "Express",
-  //       //   datas: result.rows[0].name
-  //       // });
-  //       // console.log(result); //コンソール上での確認用なため、この1文は必須ではない。
-  //     });
-  //   }
-  // });
 });
 
 app.post("/caps", (req, res) => {
@@ -67,14 +45,14 @@ app.post("/caps", (req, res) => {
     widthSets.forEach((widthSet, index) => {
       promiseList.push(
         (async () => {
-          const page = await browser.newPage();
+          const page = await browser.newPage(); //新しいタブを開く
           if (widthSet !== "full") {
             await page.setViewport({
               width: widthSet,
               height: 768
             });
           }
-          const puppeteerRes = await page.goto(requestUrl01, DCL);
+          const puppeteerRes = await page.goto(requestUrl01, DCL); //指定したURLに移動
           if (puppeteerRes.status() !== 200)
             return `${puppeteerRes.status()} ERROR`;
 
@@ -93,41 +71,13 @@ app.post("/caps", (req, res) => {
       vList.forEach(title => titleList.push(title));
     });
 
-    // const page = await browser.newPage(); //新しいタブを開く
-    // await page.goto(requestUrl01); //指定したURLに移動
-    // await page.screenshot({
-    //   path: `static/images/${capsId}/01.png`,
-    //   fullPage: true
-    // }); //スクリーンショットを撮る
-    // await page.goto(requestUrl02); //指定したURLに移動
-    // await page.screenshot({
-    //   path: `static/images/${capsId}/02.png`,
-    //   fullPage: true
-    // }); //スクリーンショットを撮る
-
     await browser.close(); //Chromiumを閉じる
 
     await Page.build({
       id: recentId + 1,
-      // counter: 1,
       processing: true,
       url: capsId
-      // createdAt: new Date(),
-      // updatedAt: new Date()
     }).save();
-
-    // await Staff.build({
-    //   id: 4,
-    //   name: "tarou",
-    //   age: 21
-    // }).save();
-
-    // await Staff.create({
-    //   name: "tarou",
-    //   age: 21
-    // }).then(staff => {
-    //   res.redirect(`/${capsId}`);
-    // });
 
     await res.redirect(`/${capsId}`); // awaitしてリダイレクトしないとページ遷移時に画像が表示されないため。（スクリーンショットが撮り終わったタイミングの処理をvueに記述すればうまくいくかも？）
   })();
