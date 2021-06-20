6d006ab89a38a341d198bbf8f0629bcc5d0bbdfa
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-06-20 21:58:50 +0900

    titleタグ変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "Caputure Plus",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
